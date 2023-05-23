f527f589333e43c3880bacd57e6756c50d193dcf
SergioRuval <ruvaloser@gmail.com>
2023-05-23 11:22:06 -0600

    ADD gridSquare class styles to generated divs

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const canvas = document.querySelector('#canvas');
 function createGrid(){
     for(let i = 0; i < 16; i++){
         const canvasSquare = document.createElement('div');
-        canvasSquare.style.border = "1px solid black";
-        canvasSquare.style.height = "5px";
-        canvasSquare.style.width = "5px";
+        canvasSquare.classList.add('gridSquare');
         canvas.appendChild(canvasSquare);
     }
 }
