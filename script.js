455328ee03b00259e5b2ea15a1e7d94093416303
SergioRuval <ruvaloser@gmail.com>
2023-05-23 11:28:37 -0600

    CREATE grid using flexbox

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const canvas = document.querySelector('#canvas');
 
 function createGrid(){
     for(let i = 0; i < 16; i++){
-        const canvasSquare = document.createElement('div');
-        canvasSquare.classList.add('gridSquare');
-        canvas.appendChild(canvasSquare);
+        const canvasRow = document.createElement('div');
+        canvasRow.classList.add('gridRow');
+        for(let j = 0; j < 16; j++){
+            const canvasSquare = document.createElement('div');
+            canvasSquare.classList.add('gridSquare');
+            canvasRow.appendChild(canvasSquare);
+        }
+        canvas.appendChild(canvasRow);
     }
 }
 
